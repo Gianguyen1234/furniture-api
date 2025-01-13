adf0bfb11222709a11ea247309ee718f732812d0
Thao Nguyen <nguyenluthao252@gmail.com>
2025-01-13 15:55:40 +0700

    cmt

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -14,7 +14,8 @@ const router = express.Router();
 router.post('/', authMiddleware, roleMiddleware('admin'), createCoupon); // Admin creates a coupon
 router.get('/', authMiddleware, roleMiddleware('admin'), getAllCoupons); // Admin fetches all coupons
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), deleteCoupon); // Admin deletes a coupon
-router.get('/promotions', getActivePromotions); // Get active promotions
-router.post('/apply', authMiddleware, applyCoupon); // Apply a coupon
+
+router.get('/promotions', getActivePromotions); // Get active promotions ( no need to be authenticated)
+router.post('/apply', authMiddleware, applyCoupon); // Apply a coupon (for users)
 
 module.exports = router;
